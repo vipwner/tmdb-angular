e2fa1e1f67816abead4abeb5bfecb7f8ecb436e1
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-13 23:02:59 +0000

    Service appendices examples added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,14 @@ export class AppComponent {
   selectedHero: Hero;
   
   constructor(private heroService: HeroService) { }
+  // getHeroes(): void {
+  // this.heroes = this.heroService.getHeroes();
+  // }
   getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
   ngOnInit(): void {
-  this.getHeroes();
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
