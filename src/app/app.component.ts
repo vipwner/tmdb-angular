fdfeb8947d961dd452a3d9beea6268d590eeccf2
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-13 14:46:20 +0000

    First steps with the Angular 4 Tutorial, Quickstart setup.

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent  { 
+  title = "Tour of Heroes";
+    
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
