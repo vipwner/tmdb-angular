74166f81cb8b71cf82e428f7e720dda9504e9570
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-13 16:59:08 +0000

    Master/Detail step of Angular 4 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
+ 
 export class Hero {
   id: number;
   name: string;
-  
 }
-
+ 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -18,18 +17,18 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
+ 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent  { 
-  title = "Tour of Heroes";
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes=HEROES;
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
-
