5d04c52651165bedb80aec9cf5ee2e0568295a2c
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-13 15:59:17 +0000

    Adding style to the heroes's list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,7 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent  { 
   title = "Tour of Heroes";
