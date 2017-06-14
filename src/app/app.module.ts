35ff8f6c2c4b81396e4d9cdca25bec93d8ae4a4b
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-14 22:12:59 +0000

    Some changes to fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
  
 import { AppComponent }        from './app.component';
+import { DashboardComponent}   from './partials/dashboard/dashboard.component';
 import { HeroDetailComponent } from './partials/hero/hero-detail.component';
 import { HeroesComponent }     from './partials/hero/heroes.component';
 import { HeroService }         from './services/hero.service';
@@ -10,10 +12,22 @@ import { HeroService }         from './services/hero.service';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
