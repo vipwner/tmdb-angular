bd2536f105ae7a88ed385fc2eb3e175e15813d70
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-15 14:50:48 +0000

    Update the HeroesComponent class done - Tour of heroes' step.

--- src/app/partials/hero/heroes.component.ts
+++ src/app/partials/hero/heroes.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+// Added OnInit - Update the HeroesComponent class step
+import { Component , OnInit} from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './../../services/hero.service';
+// Added import - Update the HeroesComponent class step
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
   styleUrls:['./heroes.component.css']
 })
-export class HeroesComponent {
+// Added OnInit - Update the HeroesComponent class step
+export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
   
-  constructor(private heroService: HeroService) { }
+  constructor(private router: Router, private heroService: HeroService) { }
   getHeroes(): void {
-  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
   ngOnInit(): void {
-  this.getHeroes();
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
