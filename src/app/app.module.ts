c67203522db8cc6e02c798858653bd6edf5f5127
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-15 14:38:02 +0000

    Refactor routes done - Tour of heroes' step.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+
+// Erase this import - Refactor routes step
+//import { RouterModule }   from '@angular/router';
+
 // Components.
 import { AppComponent }        from './app.component';
 import { DashboardComponent } from './partials/dashboard/dashboard.component';
@@ -9,26 +12,32 @@ import { HeroDetailComponent } from './partials/hero/hero-detail.component';
 import { HeroesComponent }     from './partials/hero/heroes.component';
 import { HeroService }         from './services/hero.service';
 
+// Added this import - Refactor routes step
+import { AppRoutingModule }     from './app-routing.module';
+
  
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      // Parameterized -- step
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule
+    
+    // Erase this routing section - Refactor routes step. It's unneeded.
+    // RouterModule.forRoot([
+    //   {
+    //     path: 'heroes',
+    //     component: HeroesComponent
+    //   },
+    //   {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    //   },
+    //   // Parameterized -- step
+    //   {
+    //     path: 'detail/:id',
+    //     component: HeroDetailComponent
+    //   },
+    //])
   ],
   declarations: [
     AppComponent,
@@ -36,9 +45,7 @@ import { HeroService }         from './services/hero.service';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
