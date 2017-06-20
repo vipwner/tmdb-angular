9da29f6e50843f3820577ce3344abc9876bab189
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-20 15:55:40 +0000

    Create movie-detail's component and service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
