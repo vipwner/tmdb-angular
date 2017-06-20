830ec60bd9a6b2c0ee012729bba1d65d3193eaf0
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-20 18:17:17 +0000

    Set app module configuration for components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+// New components.
 import { HeaderComponent } from './header/header.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+// Services.
+import { MovieDetailService } from './movie-detail/movie-detail.service';
+// Routing.
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MovieDetailComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [MovieDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
