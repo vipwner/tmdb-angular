3d69acb15ea8580f915d018c092d3206879b2504
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-20 16:08:26 +0000

    Set up API's key and API's URL

--- src/app/movie-detail/movie-detail.service.ts
+++ src/app/movie-detail/movie-detail.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MovieDetailService {
+    
+/**
+* API's URL
+**/
+baseUrl: string = "https://api.themoviedb.org/3/movie";
+/**
+* Jhon's API-key
+**/
+apiKey: string = "6dc0d2605088c01254ffedbd444bc2e4";
 
-  constructor() { }
+constructor() { }
 
 }
