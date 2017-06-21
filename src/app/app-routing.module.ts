23688472701077e674fd38bab609363a7bbe95f3
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-21 23:02:49 +0000

    Delete movie's route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MoviesComponent } from './movies/movies.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: HomeComponent },
-  { path: 'movies', component: MoviesComponent}
+  { path: 'home',  component: HomeComponent }
+  
 ];
 
 @NgModule({
