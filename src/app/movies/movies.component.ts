0165f3652f1d1c9e26637aba60217760968743f7
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-21 03:06:41 +0000

    Set movies component for request the API

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+ 
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
+ 
+// Observable class extensions
+import 'rxjs/add/observable/of';
+ 
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import { TmdbService } from './../services/tmdb.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+
+  movies = [];
+  constructor(private tmdbService:TmdbService, private router:Router) { }
+
+  ngOnInit() {
+    console.log('netro');
+    this.tmdbService.getMovies().subscribe(response => {
+						this.movies = response;
+            console.log("entro",response);
+    });
+  }
+  
+
+}
