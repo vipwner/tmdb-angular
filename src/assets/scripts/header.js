1bb8d8e0119d01ea22ad1b8910353397860fbce0
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-21 23:12:27 +0000

    Adds header javascript functionality

--- src/assets/scripts/header.js
+++ src/assets/scripts/header.js
@@ -4,7 +4,7 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
 
-$(window).scroll(function(event) {
+$(window).scroll(function(event){
     didScroll = true;
 });
 
@@ -17,23 +17,22 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-
+    
     // Make sure they scroll more than delta
-    if (Math.abs(lastScrollTop - st) <= delta)
+    if(Math.abs(lastScrollTop - st) <= delta)
         return;
-
+    
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight) {
+    if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
-    }
-    else {
+    } else {
         // Scroll Up
-        if (st + $(window).height() < $(document).height()) {
+        if(st + $(window).height() < $(document).height()) {
             $('header').removeClass('nav-up').addClass('nav-down');
         }
     }
-
+    
     lastScrollTop = st;
 }
