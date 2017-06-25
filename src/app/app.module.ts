a24c4d97b981951012b9c6e952f29519e82cd41e
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-25 01:54:31 +0000

    Adds search's component and service implementation base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 // New components.
 import { NavbarComponent } from './navbar/navbar.component';
 // Services.
@@ -10,6 +11,7 @@ import { TmdbImgService } from './services/tmdb-img.service';
 import { PeopleService } from './services/people.service';
 import { PeopleDetailService } from './services/people-detail.service';
 import { MovieDetailService } from './services/movie-detail.service';
+import { SearchService } from './services/search.service';
 // Routing.
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +22,7 @@ import {ScrollToModule} from 'ng2-scroll-to';
 import { PeopleComponent } from './people/people.component';
 import { PeopleDetailComponent } from './people-detail/people-detail.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -30,20 +33,23 @@ import { MovieDetailComponent } from './movie-detail/movie-detail.component';
     ContentComponent,
     PeopleComponent,
     PeopleDetailComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    SearchComponent
     
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    ScrollToModule.forRoot()
+    ScrollToModule.forRoot(),
+    FormsModule
   ],
   providers: [TmdbService,
   TmdbImgService,
   PeopleService,
   PeopleDetailService,
-  MovieDetailService],
+  MovieDetailService,
+  SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
