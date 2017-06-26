cc78b469becfcf4a139aa2d658221eb3f6cfbbe4
Jhon <jhon.pantoja@correounivalle.edu.co>
2017-06-25 23:50:58 -0500

    Adds content's component documentation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * Content's component
+ *
+ * Principal component of app's content
+ * @author Jhon Pantoja <jhon.pantoja@correounivalle.edu.co>
+ *
+ */
 
-@Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
-})
+// Core imports
+import {Component, OnInit} from '@angular/core';
+
+@Component({selector: 'app-content', templateUrl: './content.component.html', styleUrls: ['./content.component.css']})
+
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
